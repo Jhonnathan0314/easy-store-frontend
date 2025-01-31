0295485966b139e22cab0b223bb94ca9dda981f2
Jhonnathan0314 <jffc314@gmail.com>
2025-01-30 19:27:01 -0500

    Migrate interceptor

--- src/app.config.ts
+++ src/app.config.ts
@@ -2,13 +2,13 @@ import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { ApplicationConfig } from "@angular/core";
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from "@angular/router";
-import { ApiToken } from "@interceptor/api-token/api-token.interceptor"; //TODO
+import { apiToken } from "@interceptor/api-token/api-token.interceptor";
 import { routes } from "./app/app.routes";
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes),
-        provideHttpClient(withInterceptors([])),
+        provideHttpClient(withInterceptors([apiToken])),
         provideAnimations()
     ]
 }
--- src/app/core/interceptors/api-token/api-token.interceptor.ts
+++ src/app/core/interceptors/api-token/api-token.interceptor.ts
@@ -1,23 +1,17 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
+import { inject } from "@angular/core";
 import { SessionService } from "../../services/session/session.service";
 
-@Injectable()
-export class ApiToken implements HttpInterceptor {
+export const apiToken: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
+    if(req.url.includes("auth")) return next(req.clone());
 
-    constructor(private sessionService: SessionService) { }
+    const sessionService = inject(SessionService);
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if(req.url.includes("auth")) return next.handle(req.clone());
-        
-        const token = this.sessionService.getToken();
+    const token = sessionService.getToken();
 
-        const authReq = req.clone({
-            headers: req.headers.set('Authorization', `Bearer ${token}`)
-        });
-
-        return next.handle(authReq);
-    }
-
+    const authReq = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${token}`)
+    });
+    
+    return next(authReq);
 }
