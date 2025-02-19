39ecf45d4a376c293affcd8f5c7846a70eeca9d8
Jhonnathan0314 <jffc314@gmail.com>
2025-02-19 14:50:10 -0500

    Remove unused components

--- src/app/module/dashboard/pages/store/store.routes.ts
+++ src/app/module/dashboard/pages/store/store.routes.ts
@@ -1,13 +1,9 @@
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
-import { CreateProductComponent } from './create-product/create-product.component';
-import { UpdateProductComponent } from './update-product/update-product.component';
 import { CartComponent } from './cart/cart.component';
 
 export const routes: Routes = [
-  { path: '', component: ProductsComponent },
-  { path: 'create', component: CreateProductComponent },
-  { path: 'update', component: UpdateProductComponent },
+  { path: ':_id', component: ProductsComponent },
   { path: 'cart', component: CartComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
