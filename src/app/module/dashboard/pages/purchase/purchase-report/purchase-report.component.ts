4f791dd9495ceb4e845035cd44c6b7019fd22007
Jhonnathan0314 <jffc314@gmail.com>
2025-02-19 14:41:12 -0500

    Implement purchase report with basic charts

--- src/app/module/dashboard/pages/purchase/purchase-report/purchase-report.component.ts
+++ src/app/module/dashboard/pages/purchase/purchase-report/purchase-report.component.ts
@@ -1,32 +1,91 @@
-import { Component, OnInit } from '@angular/core';
-import { ButtonComponent } from '../../../../../shared/inputs/button/button.component';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { Category } from '@models/data/category.model';
+import { DataObject } from '@models/utils/object.data-view.model';
+import { get } from 'node:http';
 import { ChartModule } from 'primeng/chart';
 
 @Component({
   selector: 'app-purchase-report',
   standalone: true,
-  imports: [RouterModule, ButtonComponent, ChartModule],
+  imports: [RouterModule, ChartModule],
   templateUrl: './purchase-report.component.html',
 })
 export class PurchaseReportComponent implements OnInit {
   
-  basicData: any;
-  basicOptions: any;
+  @Input() purchases: DataObject[] = [];
+  @Input() categories: Category[] = [];
+
+  valueData: any;
+  numData: any;
+
+  valueLabels: string[] = ['2024-11', '2024-12', '2025-01', '2025-02']
+  numLabels: string[] = []
 
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    this.basicData = {
-      labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+    this.graphicData();
+    console.log({purchases: this.purchases});
+  }
+
+  graphicData() {
+    const valueData = this.getValueData();
+    const numData = this.getNumData();
+
+    this.valueData = {
+      labels: this.valueLabels,
       datasets: [
         {
-          label: 'Sales',
-          data: [540, 325, 702, 620],
+          label: 'Compras',
+          data: valueData,
           borderWidth: 1,
         },
       ],
     };
+
+    this.numData = {
+      labels: this.numLabels,
+      datasets: [
+        {
+          label: 'Compras',
+          data: numData,
+          borderWidth: 1,
+        },
+      ],
+    };
+  }
+
+  getValueData() {
+    let data = this.valueLabels.map(() => 0);
+
+    this.purchases.forEach(purchase => {
+      if(purchase.purchase) {
+        const date = new Date(purchase.purchase.creationDate);
+        const yearMonth = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}`;
+    
+        const index = this.valueLabels.indexOf(yearMonth);
+        if (index !== -1) {
+          data[index] += purchase.purchase.total;
+        }
+      }
+    });
+    return data;
+  }
+
+  getNumData() {
+    this.numLabels = this.categories.map(category => category.name);
+    let data = this.numLabels.map(() => 0)
+
+    this.purchases.forEach(purchase => {
+      if(purchase.purchase) {
+        if(purchase.purchase.category) {
+          const index = this.numLabels.indexOf(purchase.purchase.category.name);
+          data[index] += 1;
+        }
+      }
+    });
+    return data;
   }
 
   goBack() {
