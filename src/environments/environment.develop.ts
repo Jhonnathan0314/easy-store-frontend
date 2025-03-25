a839dcc434c41cc0d81c29c4099f19adde6b3c7e
Jhonnathan0314 <jffc314@gmail.com>
2025-03-25 08:57:02 -0500

    Implement environment develop

--- src/environments/environment.develop.ts
+++ src/environments/environment.develop.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    BACKEND_URL: 'https://easy-store-backend-production.up.railway.app',
+    BACKEND_URL: 'http://localhost:8080',
     BACKEND_PATH: '/api/v1',
     CRYPTO_KEY: '643D771DA7AA038F3670CB9206CD9B9E5988BAFEB7975D079CF42BD09A09DC3F'
 };
