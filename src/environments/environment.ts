3839323ff4141f92b3814c494039e91a44f37e69
Jhonnathan0314 <jffc314@gmail.com>
2025-03-24 23:10:59 -0500

    Set backend url with http

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     // BACKEND_URL: 'http://localhost:8080',
-    BACKEND_URL: 'https://easy-store-backend.railway.internal',
+    BACKEND_URL: 'http://easy-store-backend.railway.internal',
     BACKEND_PATH: '/api/v1',
     CRYPTO_KEY: '643D771DA7AA038F3670CB9206CD9B9E5988BAFEB7975D079CF42BD09A09DC3F'
 };
