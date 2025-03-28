33b3c4ecff7998b48dd2aac887f54a3329c67c14
Jhonnathan0314 <jffc314@gmail.com>
2025-03-27 21:51:36 -0500

    Create whatsapp pipe

---
+++ src/app/core/pipes/whatsapp/whatsapp.pipe.ts
@@ -0,0 +1,14 @@
+import { PurchaseCart } from "@models/data/purchase.model";
+
+export class WhatsappPipe {
+    public getProductsText(cart: PurchaseCart): string {
+        const products = cart.products;
+        const textArray = products.map(product => {
+            return `%2D%20${product.product?.name.replaceAll(' ', '%20')}%3A%20${product.quantity}%20unidad%2Fes%20%3D%3E%20Subtotal%3A%20${product.subtotal}%20%0A`;
+        })
+        let productsText = "";
+        textArray.forEach(txt => productsText += txt);
+        productsText += `Valor%20total%3A%20%24%20${cart.total}`;
+        return productsText;
+    }
+}
