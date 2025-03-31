c449b9eb928ee6db13e5c4c8868115b38cea4eb9
Jhonnathan0314 <jffc314@gmail.com>
2025-03-30 21:33:22 -0500

    Update whatsapp number

--- src/app/core/services/utils/data/static-data/static-data.service.ts
+++ src/app/core/services/utils/data/static-data/static-data.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class StaticDataService {
 
-  private CART_MESSAGE: string = "https://wa.me/+573125543042?text=Hola!%20%0A%0ATengo%20el%20carrito%20numero%20{cartId}%20y%20quisiera%20realizar%20la%20compra%20de%20los%20articulos%20incluidos%20en%20el.%20%0A%0ALos%20productos%20son%20%0A{products}";
+  private CART_MESSAGE: string = "https://wa.me/+573202778890?text=Hola!%20%0A%0ATengo%20el%20carrito%20numero%20{cartId}%20y%20quisiera%20realizar%20la%20compra%20de%20los%20articulos%20incluidos%20en%20el.%20%0A%0ALos%20productos%20son%20%0A{products}";
 
   constructor() { }
 
